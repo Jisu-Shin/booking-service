47993ab7bbaf6a062e53776425ebe2dc38303f32
Jisu-Shin <sjszzang3053@gmail.com>
2025-04-24 03:43:09 +0900

    Test : 테스트 코드 추가

--- src/main/java/com/booking_service/dto/BookingCreateRequestDto.java
+++ src/main/java/com/booking_service/dto/BookingCreateRequestDto.java
@@ -1,11 +1,13 @@
 package com.booking_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BookingCreateRequestDto {
     private long custId;
     private long itemId;
